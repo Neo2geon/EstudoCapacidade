29e684b5ea3fc2a5cad046a25d641ee9260ccfeb
Stevan Lopes <neo2geon@gmail.com>
2021-10-23 15:59:18 -0300

    Teste do segundo commit

--- capacidade.py
+++ capacidade.py
@@ -1,2 +1,11 @@
+from unittest.mock import inplace
+
 import pandas as pd
 
+df = pd.read_csv('DadosCSV.csv', delimiter=';', index_col=0)
+df.replace([','], '.', inplace=True)
+print(df.head())
+
+
+
+#media = df['Position A5'].mean()
