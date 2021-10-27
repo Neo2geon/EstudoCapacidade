d11631566dec5bc293496437fa65b25aa1054472
Stevan Lopes <neo2geon@gmail.com>
2021-10-26 22:50:03 -0300

    Feito: Criado classe capacidade, criado atributos LSE, LIE, métodos de média, desvio padrão e índice CP Fazer: calculo valor 1

--- capacidade.py
+++ capacidade.py
@@ -1,10 +1,17 @@
-import pandas as pd
-from replaceCSV import replaceCSV
+import statistics
 
+class EstudoCapacidade:
+    def __init__(self, lse: float, lie: float, label_dados: str, matriz_dados: 'lista de dados'):
+        self.lse = lse
+        self.lie = lie
+        self.label_dados = label_dados
+        self.matriz_dados = matriz_dados
 
-arquivo_de_dados = "DadosCSV.csv"
-replaceCSV(arquivo_de_dados, ',', '.')
+    def media(self):
+        return statistics.mean(self.matriz_dados)
 
-df = pd.read_csv(arquivo_de_dados, delimiter=';', index_col=0)
-print(df.head())
+    def desviopadrao(self):
+        return statistics.stdev(self.matriz_dados)
 
+    def indiceCP(self):
+        return (self.lse - self.lie) / (6 * self.desviopadrao())
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from builtins import object
 
+import pandas as pd
+from replaceCSV import replaceCSV
+from capacidade import EstudoCapacidade
+
+arquivo_de_dados = "DadosCSV.csv"
+replaceCSV(arquivo_de_dados, ',', '.')
+
+df = pd.read_csv(arquivo_de_dados, delimiter=';', index_col=0)
+print(df.head())
+
+matrizDados = [10, 20, 30]
+LSE = 30
+LIE = 10
+
+objCapacidade = EstudoCapacidade(LSE, LIE, 'Position B1', matrizDados)
+print(objCapacidade.media())
+print(objCapacidade.desviopadrao())
+print(objCapacidade.indiceCP())
