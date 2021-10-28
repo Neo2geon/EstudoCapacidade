7536e77c6727c558de56fe1ca4dadccf7f9d4084
Stevan Lopes <neo2geon@gmail.com>
2021-10-27 22:01:58 -0300

    Feito: Transforma a coluna 1 do df em lista e passa no parâmetro da classe EstudoCapacidade Fazer: calculo valor 1

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from builtins import object
-
 import pandas as pd
 from replaceCSV import replaceCSV
 from capacidade import EstudoCapacidade
@@ -8,13 +6,15 @@ arquivo_de_dados = "DadosCSV.csv"
 replaceCSV(arquivo_de_dados, ',', '.')
 
 df = pd.read_csv(arquivo_de_dados, delimiter=';', index_col=0)
-print(df.head())
+#print(df.head())
 
-matrizDados = [10, 20, 30]
 LSE = 30
 LIE = 10
 
-objCapacidade = EstudoCapacidade(LSE, LIE, 'Position B1', matrizDados)
-print(objCapacidade.media())
-print(objCapacidade.desviopadrao())
-print(objCapacidade.indiceCP())
+objCapacidade = EstudoCapacidade(LSE, LIE, 'Position B1', df.iloc[:, 1].tolist())
+
+print("Média: " + str(objCapacidade.media()))
+print("Desvio Padrão: " + str(objCapacidade.desviopadrao()))
+print("Índice CP: " + str(objCapacidade.indiceCP()))
+
+
