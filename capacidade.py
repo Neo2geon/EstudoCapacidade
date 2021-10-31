4ecc78838bad9246e8f21c22b3e0f0c0a6d7ae57
Stevan Lopes <neo2geon@gmail.com>
2021-10-31 16:09:40 -0300

    Feito: Gera o relatório final em txt baseado na data e hora atual Fazer: Gráficos

--- capacidade.py
+++ capacidade.py
@@ -1,4 +1,5 @@
 import statistics
+from datetime import datetime
 
 class EstudoCapacidade:
     def __init__(self, lse: float, lie: float, label_dados: str, matriz_dados: 'lista de dados'):
@@ -24,3 +25,22 @@ class EstudoCapacidade:
 
     def cpk(self):
         return min(self.valor1(), self.valor2())
+
+    def relatorio_final(self):
+        texto_final = '***********************          ESTUDO DE CAPACIDADE          ***********************'
+        texto_final += '\n'
+        texto_final += '***********************              ' + self.label_dados + '               ***********************'
+        texto_final += '\n\nDados:'
+        for a in self.matriz_dados:
+            texto_final += '\n' + str(a)
+        texto_final += '\n\n\nEstatísticas:'
+        texto_final += '\n - Média: ' + str(self.media())
+        texto_final += '\n - Desvio Padrão: ' + str(self.desviopadrao())
+        texto_final += '\n - Índice CP: ' + str(self.indiceCP())
+        texto_final += '\n - CPK: ' + str(self.cpk())
+        return texto_final
+
+    def grava_arquivo_relatorio_final(self, texto_final: str):
+        nome_arquivo = 'relatorio ' + str(datetime.now().strftime('%d_%m_%Y %H_%M_%S')) + '.txt'
+        with open(nome_arquivo, 'w') as arquivo:
+            arquivo.write(texto_final)
--- main.py
+++ main.py
@@ -8,15 +8,20 @@ replaceCSV(arquivo_de_dados, ',', '.')
 df = pd.read_csv(arquivo_de_dados, delimiter=';', index_col=0)
 #print(df.head())
 
-LSE = 1.4
-LIE = 1.2
+LSE = 1.2
+LIE = 1.1
 
 colunaDoDf = 'Position A5'
 
 listaDeDados =  df[colunaDoDf].tolist()
 objCapacidade = EstudoCapacidade(LSE, LIE, colunaDoDf, listaDeDados)
 
+objCapacidade.grava_arquivo_relatorio_final(objCapacidade.relatorio_final())
+
+'''
+print(objCapacidade.monta_relatorio())
 print("Média: " + str(objCapacidade.media()))
 print("Desvio Padrão: " + str(objCapacidade.desviopadrao()))
 print("Índice CP: " + str(objCapacidade.indiceCP()))
 print("CPK: " + str(objCapacidade.cpk()))
+'''
